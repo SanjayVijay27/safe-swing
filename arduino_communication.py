eeedb6e92d46d033285cbb563fa9a5e4c6f7da29
Trimineman <westfordjack@gmail.com>
2024-03-03 13:10:27 -0500

    Frame working

--- arduino_communication.py
+++ arduino_communication.py
@@ -1,6 +1,32 @@
 from Arduino import Arduino
 import time
-board = Arduino(port="COM9")
 
-board.Servos.attach(9)
-board.Servos.write(9,180)
+class ArduinoCommunication:
+    def __init__(self):
+        self.board = Arduino(port="COM9")
+        self.board.Servos.attach(9)
+        self.board.Servos.write(9,180)
+
+        self.unlocked = False
+    
+    def lock_door(self):
+        if self.unlocked:
+            self.board.Servos.attach(9)
+            self.board.Servos.write(9, 180)
+            self.unlocked = False
+
+    def unlock_door(self):
+        self.board.Servos.detach(9)
+        self.unlocked = True
+
+a = ArduinoCommunication()
+print("locked")
+a.lock_door()
+time.sleep(5)
+print("unlocked")
+a.unlock_door()
+time.sleep(5)
+print("locked")
+a.lock_door()
+time.sleep(5)
+print("end")
--- frame.py
+++ frame.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
+import image_detector
 
 #define colors
 bg_color = "#121212"
@@ -20,8 +21,8 @@ height = 500
 
 #create main window
 main_window = Tk()
-icon = ImageTk.PhotoImage(file="logo.png")
-main_window.wm_iconphoto(False, icon)
+#icon = ImageTk.PhotoImage(file="logo.png")
+#main_window.wm_iconphoto(False, icon)
 main_window.title("Safe-Swing")
 main_window.geometry(str(width) + "x" + str(height))
 main_window.config(bg=level1_color)
@@ -33,76 +34,40 @@ title_frame.grid(row=0, column=0, pady=20)
 title_frame.pack_propagate(False)
 
 #create title label
-title_label = Label(title_frame,text="Nutrireader",font=("Satoshi", '30', 'bold'),bg=level1_color,fg=primary_color)
+title_label = Label(title_frame,text="Safe-Swing",font=("Satoshi", '30', 'bold'),bg=level1_color,fg=primary_color)
 title_label.pack()
 
 #create description label
-desc_label = Label(title_frame,text="Nutrition with Nutrireader makes you the leader!",font=("Satoshi", '12', 'bold italic'),bg=level1_color,fg=tertiary_color)
+desc_label = Label(title_frame,text="Save your door and save others!",font=("Satoshi", '12', 'bold italic'),bg=level1_color,fg=tertiary_color)
 desc_label.pack()
-
-#create frames, labels, and entries for ingredients
-data_frame = Frame(main_window, bg=level1_color)
-data_frame.grid(row=1, column=0)
-
-#radio buttons for type of item
-item_type = IntVar()
-food_option_frame = Frame(data_frame, bg=level1_color)
-food_option_frame.grid(row = 7, column=0, columnspan=3)
-type_label = Label(food_option_frame, font=("Satoshi", 10), text="Item Type: ", fg = 'white', bg=level1_color)
-type_label.grid(row=0,column=0,sticky="E")
-food_button = Radiobutton(food_option_frame, text = "Food", variable=item_type, value = 0, font = ("Satoshi", 10), fg=tertiary_color, bg=level1_color, activeforeground=primary_color, activebackground=level1_color, selectcolor=level1_color)
-food_button.grid(row=0,column=1)
-drink_button = Radiobutton(food_option_frame, text = "Drink", variable=item_type, value = 1, font = ("Satoshi", 10), fg=tertiary_color, bg=level1_color, activeforeground=primary_color, activebackground=level1_color, selectcolor=level1_color)
-drink_button.grid(row=0,column=2)
-
-#radio buttons for verboseness
-explanation_type = IntVar()
-ex_option_frame = Frame(data_frame, bg=level1_color)
-ex_option_frame.grid(row = 8, column=0, columnspan=3)
-ex_label = Label(ex_option_frame, font=("Satoshi", 10), text="Explanation Length: ", bg=level1_color, fg = 'white')
-ex_label.grid(row=0,column=0,sticky="E")
-short_button = Radiobutton(ex_option_frame, text = "Short", variable=explanation_type, value = 0, font = ("Satoshi", 10), fg=tertiary_color, bg=level1_color, activeforeground=primary_color, activebackground=level1_color, selectcolor=level1_color)
-short_button.grid(row=0,column=1)
-med_button = Radiobutton(ex_option_frame, text = "Medium", variable=explanation_type, value = 1, font = ("Satoshi", 10), fg=tertiary_color, bg=level1_color, activeforeground=primary_color, activebackground=level1_color, selectcolor=level1_color)
-med_button.grid(row=0,column=2)
-long_button = Radiobutton(ex_option_frame, text = "Long", variable=explanation_type, value = 2, font = ("Satoshi", 10), fg=tertiary_color, bg=level1_color, activeforeground=primary_color, activebackground=level1_color, selectcolor=level1_color)
-long_button.grid(row=0,column=3)
 
 #image vars
 webcam = cv2.VideoCapture(0)
+detector = image_detector.ObjectDetection()
 
-#open camera function
-def open_cam():
-    #show frames of camera
-    def show_frames():
-        result, raw_image = webcam.read()
-        if result:
-            converted_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
-            selected_image = Image.fromarray(converted_image)
-            final_image = ImageTk.PhotoImage(selected_image)
-            display_frame.imgtk = final_image
-            display_frame.config(image=final_image)
+#frame for camera
+object_view = Label(main_window, bg='blue', width=640, height=480)
 
-        #animate frame
-        display_frame.after(10, show_frames)
-    #initialize frame
-    cam_window = Toplevel(main_window)
-    cam_window.config(bg=level1_color)
-    cam_window.wm_iconphoto(False, icon)
-    display_frame = Label(cam_window)
-    display_frame.pack()
-    capture_button = Button(cam_window, font=('Satoshi', '12', 'normal'), text="Capture", bg=level4_color, fg=error_color, activebackground=level4_color, activeforeground=primary_color, width=50, command=capture)
-    capture_button.pack()
-    show_frames()
-    cam_window.mainloop()
+def show_feed():
+    img_ready, raw_image = webcam.read()
+    if img_ready:
+        objdata, final_image = detector(raw_image)
+        final_image = raw_image
+        processed_image = Image.fromarray(final_image)
+        processed_image = ImageTk.PhotoImage(processed_image)
+        object_view.imgtk = processed_image
+        object_view.config(image=processed_image)
 
-#Camera button
-submit_button = Button(data_frame, font=('Satoshi', '12', 'normal'), text="Open Camera for Scanning", bg=level4_color, fg=tertiary_color, activebackground=level4_color, activeforeground=primary_color, width=50, command=open_cam)
-submit_button.grid(row=10, column=0, columnspan=3)
+    object_view.after(100, show_feed)
+
+object_view.grid(row=1, column=0, pady=20)
+        
 
 #Credits
 credits_label = Label(main_window, text="Project for Blueprint 2024 by Sanjay Vijay and Jack Whitman", font=('Satoshi', '10', 'italic'), bg = level1_color, fg = secondary_color, pady=20)
 credits_label.grid(row=2, column=0)
+
+show_feed()
 
 #display main window
 main_window.mainloop()
